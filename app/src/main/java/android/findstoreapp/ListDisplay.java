4be3e90c2240bc5e16fb283c9167011461b61404
hieunguyen <kiddykooly@gmail.com>
2016-04-28 23:14:19 +0700

    update version 1.2

--- app/src/main/java/android/findstoreapp/ListDisplay.java
+++ app/src/main/java/android/findstoreapp/ListDisplay.java
@@ -10,12 +10,15 @@ import android.findstoreapp.GPSHandler.GPSHandler;
 import android.findstoreapp.adapter.StoresListAdapter;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.annotation.NonNull;
 import android.support.annotation.UiThread;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -29,6 +32,7 @@ import android.widget.Toast;
 
 import com.flipboard.bottomsheet.BottomSheetLayout;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 
 public class ListDisplay extends AppCompatActivity {
@@ -81,8 +85,7 @@ public class ListDisplay extends AppCompatActivity {
         startLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startLayout.setVisibility(View.INVISIBLE);
-                searchNearestBtn.performClick();
+                startLayout.setVisibility(View.GONE);
             }
         });
 
@@ -214,6 +217,18 @@ public class ListDisplay extends AppCompatActivity {
     }
 
     @Override
+    protected void onResume() {
+        searchNearestBtn.performClick();
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        gpsHandler.stopUsingGPS();
+        super.onStop();
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -225,12 +240,8 @@ public class ListDisplay extends AppCompatActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
